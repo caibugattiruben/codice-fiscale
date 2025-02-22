388267391bbbcc2577262c610adff64e5ea3868d
ruben <caibugatti.ruben@franchettisalviani.net>
2025-02-22 09:08:18 +0100

    codice fiscale
    fino a nome mancano X

--- CodiceFiscale/CodiceFiscale/Program.cs
+++ CodiceFiscale/CodiceFiscale/Program.cs
@@ -1,48 +1,78 @@
 ﻿string creo(string nome,string cogn, string[] data)
 {
     char[] vocali ={ 'a', 'e','i','o','u' };
-    string codice="",lettera="",vocalicogn="";
+    string codice="",lettera="",conscogn="",vocalicogn="", consnom = "", vocalinom = "";
     int si = 0,ok=0,pos=0,a=0,lung=0;
-    //cogn
-    for(int i = 0; i < cogn.Length; i++)
+    //nome
+    for(int i = 0; i < nome.Length; i++)
     {
-        if (cogn[i] == vocali[0] || cogn[i] == vocali[1] || cogn[i] == vocali[2] || cogn[i] == vocali[3] || cogn[i] == vocali[4])
+        if (!vocali.Contains(nome[i]))
+        {
+            if (consnom.Length < 3)
+            {
+                consnom += nome[i];
+            }
+            
+        }
+        else
+        {
+            vocalinom+= nome[i];
+        }
+    }
+    if(consnom.Length > 3)
+    {
+        consnom = consnom[0]+consnom.Substring(2);
+        consnom=consnom.Substring(0,3);
+    }
+    //cogn
+    for (int i = 0; i < cogn.Length; i++)
+    {
+        if (!vocali.Contains(cogn[i]))
+        {
+            if (conscogn.Length < 3)
+            {
+                conscogn += cogn[i];
+            }
+
+        }
+        else
         {
             vocalicogn += cogn[i];
         }
     }
     //COGNOME
-    while(ok!=3)
+    while (ok<=2)
     {
-        if (cogn[pos] != vocali[0] && cogn[pos] != vocali[1] && cogn[pos] != vocali[2] && cogn[pos] != vocali[3] && cogn[pos] != vocali[4])
+        codice = conscogn;
+        ok = codice.Length;
+        if (codice.Length < 3)
         {
-            lettera = cogn[pos].ToString().ToUpper();
-            codice += lettera;
-            ok++;
-            
-        }
-        else
-        {
-            
-            if (si > 0)
+           for(int i=codice.Length;i < 3; i++)
             {
-                codice += vocalicogn[a];
-                pos++;
-                a++;
+                codice += vocalicogn;
+                ok++;
+            }
+        }
+    }
+    
+    ok = 0;
+    //NOME
+    while (ok <= 2)
+    {
+        codice += consnom;
+        ok = codice.Length;
+        if (codice.Length < 6)
+        {
+            for (int i = codice.Length; i < 6; i++)
+            {
+                codice += vocalinom;
                 ok++;
                 
             }
-            
         }
-        pos++;
-        if (ok == 3 && codice.Length!=3)
-        {
-            si++;
-            ok = codice.Length;
-        }
-        lung = codice.Length;
     }
-    //NOME
+    codice=codice.Substring(0,6);
+    codice = codice.ToUpper();
     Console.WriteLine(codice);
     return codice;
 }
